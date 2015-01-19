8ab3fe8975b9b4c299fb9debedf1a5c6e1a2239b
YASME-Tim <hu78sapy@stud.cs.fau.de>
2015-01-19 21:06:13 +0100

    :pill: Fix some bash magic issues

--- port2process.sh
+++ port2process.sh
@@ -6,10 +6,10 @@ usage() { echo "Usage: ${FUNCNAME[1]} $1" && return 0; }
 PORT16="$(echo "obase=16; $PORT" | bc)"
 PORT16="$(printf "%04s" "$PORT16")"
 [ $# -eq 2 ] && echo "hex(PORT): $PORT16"
-PROTOCOL="$(adb shell 'netstat | grep '":$PORT"' | busybox awk '"'"'{print $1}'"'" | sed $'s/\r//g')"
+PROTOCOL="$(adb shell 'netstat | grep "'":$PORT"' " | busybox awk '"'"'{print $1}'"'" | sed $'s/\r//g')"
 [ -z "$PROTOCOL" ] && echo "No such open port" && exit 127
 [ $# -eq 2 ] && echo "PROTOCOL: ${PROTOCOL}"
-INODE="$(adb shell 'cat /proc/net/'"$PROTOCOL"' | grep '":$PORT16"' | busybox awk '"'"'{print $10}'"'" | sed $'s/\r//g')"
+INODE="$(adb shell 'cat /proc/net/'"$PROTOCOL"' | grep '":$PORT16 "' | busybox awk '"'"'{print $10}'"'" | sed $'s/\r//g')"
 [ $# -eq 2 ] && echo "INODE: $INODE"
 # FIXME: This somehow often results in >1 processes and will fail sometimes.
 PROCESS="$(adb shell 'for i in /proc/*/fd; do ls -l "$i" | grep '"$INODE"' &>/dev/null && echo "$i"; done' | sed $'s/\r//g')"
