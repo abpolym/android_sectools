554ee8f37b911044b67f2359295feee6734dc2ce
YASME-Tim <hu78sapy@stud.cs.fau.de>
2015-01-19 20:59:24 +0100

    :fire: Add issue fixme text

--- port2process.sh
+++ port2process.sh
@@ -10,6 +10,7 @@ PROTOCOL="$(adb shell 'netstat | grep '":$PORT"' | busybox awk '"'"'{print $1}'"
 [ $# -eq 2 ] && echo "PROTOCOL: ${PROTOCOL}"
 INODE="$(adb shell 'cat /proc/net/'"$PROTOCOL"' | grep '":$PORT16"' | busybox awk '"'"'{print $10}'"'" | sed $'s/\r//g')"
 [ $# -eq 2 ] && echo "INODE: $INODE"
+# FIXME: This somehow often results in >1 processes and will fail sometimes.
 PROCESS="$(adb shell 'for i in /proc/*/fd; do ls -l "$i" | grep '"$INODE"' &>/dev/null && echo "$i"; done' | sed $'s/\r//g')"
 PROCESS="${PROCESS#/proc/}"
 PROCESS="${PROCESS%/fd}"
