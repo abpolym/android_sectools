3159f03a5192ee5f618f813fcb0347ce77ef3592
YASME-Tim <hu78sapy@stud.cs.fau.de>
2015-01-19 21:21:59 +0100

    :muscle: Better header for ps

--- listen_port_process.sh
+++ listen_port_process.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 [ "$1" = "-v" ] && FLAG="-v"
-printf "USER\t\tPID\tPPID\tVSIZE\tRSS\tWCHAN\tPC\tNAME\n"
+printf "USER      PID   PPID  VSIZE  RSS     WCHAN    PC         NAME\n"
 for i in `adb shell netstat | grep LISTEN | awk '{print $4}' | grep --color=none -o '[0-9]:.*' | sed $'s/\r//'`; do
 	PORT="${i:2}"
 	printf "[%s]%*s\n\n" $PORT "${COLUMNS:-$(($(tput cols)-${#PORT}-2))}" '' | tr ' ' '-'
