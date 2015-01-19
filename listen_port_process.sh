ca640373c9da33e1332202d322188e810ef85fd4
YASME-Tim <hu78sapy@stud.cs.fau.de>
2015-01-19 20:43:06 +0100

    :muscle: Beautify output.

--- listen_port_process.sh
+++ listen_port_process.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 for i in `adb shell netstat | grep LISTEN | awk '{print $4}' | grep --color=none -o '[0-9]:.*' | sed $'s/\r//'`; do
-	./port2process.sh "${i:2}"
+	PORT="${i:2}"
+	printf "[%s]%*s\n\n" $PORT "${COLUMNS:-$(($(tput cols)-${#PORT}-2))}" '' | tr ' ' '-'
+	./port2process.sh "${PORT}"
+	echo
 done
+printf "%*s\n" "${COLUMNS:-$(tput cols)}" '' | tr ' ' '-'
