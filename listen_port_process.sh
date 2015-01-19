1783a9332f99d1e1f27cc7ad16ba26ed433b4afb
YASME-Tim <hu78sapy@stud.cs.fau.de>
2015-01-19 20:57:55 +0100

    :muscle: Add zero padding and better grep to solve some issues

--- listen_port_process.sh
+++ listen_port_process.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+printf "USER\t\tPID\tPPID\tVSIZE\tRSS\tWCHAN\tPC\tNAME\n"
 for i in `adb shell netstat | grep LISTEN | awk '{print $4}' | grep --color=none -o '[0-9]:.*' | sed $'s/\r//'`; do
 	PORT="${i:2}"
 	printf "[%s]%*s\n\n" $PORT "${COLUMNS:-$(($(tput cols)-${#PORT}-2))}" '' | tr ' ' '-'
-	./port2process.sh "${PORT}"
+	./port2process.sh -v "${PORT}"
 	echo
 done
 printf "%*s\n" "${COLUMNS:-$(tput cols)}" '' | tr ' ' '-'
--- port2process.sh
+++ port2process.sh
@@ -2,12 +2,13 @@
 usage() { echo "Usage: ${FUNCNAME[1]} $1" && return 0; }
 [ $# -ne 1 ] && [[ $# -ne 2 ]] && usage "[-v] <port>" && exit 1
 [ $# -eq 1 ] && PORT=$1 || PORT=$2
-[ $# -eq 2 ] && echo "$PORT"
+[ $# -eq 2 ] && echo "PORT: $PORT"
 PORT16="$(echo "obase=16; $PORT" | bc)"
-[ $# -eq 2 ] && echo "$PORT16"
-PROTOCOL="$(adb shell 'netstat | grep '"$PORT"' | busybox awk '"'"'{print $1}'"'" | sed $'s/\r//g')"
-[ $# -eq 2 ] && echo "PROTOCOL: ${#PROTOCOL}"
-INODE="$(adb shell 'cat /proc/net/'"$PROTOCOL"' | grep '"$PORT16"' | busybox awk '"'"'{print $10}'"'" | sed $'s/\r//g')"
+PORT16="$(printf "%04s" "$PORT16")"
+[ $# -eq 2 ] && echo "hex(PORT): $PORT16"
+PROTOCOL="$(adb shell 'netstat | grep '":$PORT"' | busybox awk '"'"'{print $1}'"'" | sed $'s/\r//g')"
+[ $# -eq 2 ] && echo "PROTOCOL: ${PROTOCOL}"
+INODE="$(adb shell 'cat /proc/net/'"$PROTOCOL"' | grep '":$PORT16"' | busybox awk '"'"'{print $10}'"'" | sed $'s/\r//g')"
 [ $# -eq 2 ] && echo "INODE: $INODE"
 PROCESS="$(adb shell 'for i in /proc/*/fd; do ls -l "$i" | grep '"$INODE"' &>/dev/null && echo "$i"; done' | sed $'s/\r//g')"
 PROCESS="${PROCESS#/proc/}"
