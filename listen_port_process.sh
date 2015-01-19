ba601ea8332245340f858e78bde6984bd64c668a
YASME-Tim <hu78sapy@stud.cs.fau.de>
2015-01-19 21:18:07 +0100

    :pill: Fixme issue should be fixed.

--- listen_port_process.sh
+++ listen_port_process.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+[ "$1" = "-v" ] && FLAG="-v"
 printf "USER\t\tPID\tPPID\tVSIZE\tRSS\tWCHAN\tPC\tNAME\n"
 for i in `adb shell netstat | grep LISTEN | awk '{print $4}' | grep --color=none -o '[0-9]:.*' | sed $'s/\r//'`; do
 	PORT="${i:2}"
 	printf "[%s]%*s\n\n" $PORT "${COLUMNS:-$(($(tput cols)-${#PORT}-2))}" '' | tr ' ' '-'
-	./port2process.sh -v "${PORT}"
+	[ -z "$FLAG" ] && ./port2process.sh "${PORT}" || ./port2process.sh "$FLAG" "${PORT}"
 	echo
 done
 printf "%*s\n" "${COLUMNS:-$(tput cols)}" '' | tr ' ' '-'
--- port2process.sh
+++ port2process.sh
@@ -11,9 +11,10 @@ PROTOCOL="$(adb shell 'netstat | grep "'":$PORT"' " | busybox awk '"'"'{print $1
 [ $# -eq 2 ] && echo "PROTOCOL: ${PROTOCOL}"
 INODE="$(adb shell 'cat /proc/net/'"$PROTOCOL"' | grep '":$PORT16 "' | busybox awk '"'"'{print $10}'"'" | sed $'s/\r//g')"
 [ $# -eq 2 ] && echo "INODE: $INODE"
-# FIXME: This somehow often results in >1 processes and will fail sometimes.
 PROCESS="$(adb shell 'for i in /proc/*/fd; do ls -l "$i" | grep '"$INODE"' &>/dev/null && echo "$i"; done' | sed $'s/\r//g')"
-PROCESS="${PROCESS#/proc/}"
-PROCESS="${PROCESS%/fd}"
-[ $# -eq 2 ] && echo "PROCESS: $PROCESS"
-adb shell 'ps | grep '"$PROCESS"
+echo "$PROCESS" | while read p; do 
+	p="${p#/proc/}"
+	p="${p%/fd}"
+	[ $# -eq 2 ] && echo "PROCESS: $p"
+	adb shell 'ps | grep '"$p"
+done
